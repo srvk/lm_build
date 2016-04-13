10d696b117e52508cf8009264e8821b246e1e6ff
Eric Riebling <er1k@cs.cmu.edu>
2016-04-13 22:01:20 +0000

    move contents

---
+++ train_lms.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script trains LMs.
+
+train_txt=$1
+dir=$2
+
+mkdir -p $dir
+. ./path.sh || exit 1; # for KALDI_ROOT
+export PATH=$KALDI_ROOT/tools/kaldi_lm:$PATH
+( # First make sure the kaldi_lm toolkit is installed.
+ cd $KALDI_ROOT/tools || exit 1;
+ if [ -d kaldi_lm ]; then
+   echo Not installing the kaldi_lm toolkit since it is already there.
+ else
+   echo Downloading and installing the kaldi_lm tools
+   if [ ! -f kaldi_lm.tar.gz ]; then
+     wget http://www.danielpovey.com/files/kaldi/kaldi_lm.tar.gz || exit 1;
+   fi
+   tar -xvzf kaldi_lm.tar.gz || exit 1;
+   cd kaldi_lm
+   make || exit 1;
+   echo Done making the kaldi_lm tools
+ fi
+) || exit 1;
+
+# Training data with OOV words replaced with <UNK>.
+cat $train_txt | awk -v w=wordlist.txt \
+  'BEGIN{while((getline<w)>0) v[$1]=1;} {for (i=1;i<=NF;i++) if ($i in v) printf $i" ";else printf "<UNK> ";print ""}'|sed 's/ $//g' \
+  | gzip -c > $dir/train_nounk.gz
+
+gunzip -c $dir/train_nounk.gz | cat - wordlist.txt | \
+  awk '{ for(x=1;x<=NF;x++) count[$x]++; } END{for(w in count){print count[w], w;}}' | \
+ sort -nr > $dir/unigram.counts
+
+# Get "mapped" words
+cat $dir/unigram.counts  | awk '{print $2}' | get_word_map.pl "<s>" "</s>" "<UNK>" > $dir/word_map
+
+gunzip -c $dir/train_nounk.gz | awk -v wmap=$dir/word_map 'BEGIN{while((getline<wmap)>0)map[$1]=$2;}
+  { for(n=1;n<=NF;n++) { printf map[$n]; if(n<NF){ printf " "; } else { print ""; }}}' | gzip -c >$dir/train.gz
+
+rm $dir/train_nounk.gz 
+
+# Now train and prune the LM. Her we go.
+train_lm.sh --arpa --lmtype 3gram-mincount $dir
+prune_lm.sh --arpa 6.0 $dir/3gram-mincount/
+
+# Format the generated LM
+echo "Done with LM building. Now formatting LM to FST ..."
+cp -r lang_bd $dir/lang_test_bd_tgpr
+
+gunzip -c $dir/3gram-mincount/lm_pr6.0.gz | arpa2fst - | fstprint | \
+    utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=lang_bd/words.txt \
+    --osymbols=lang_bd/words.txt  --keep_isymbols=false --keep_osymbols=false | \
+    fstrmepsilon > $dir/lang_test_bd_tgpr/G.fst || exit 1;
+
+fstisstochastic $dir/lang_test_bd_tgpr/G.fst
+
+exit 0
+
